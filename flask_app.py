9968d21c0034ccf9388d45d625a056cee97a01d6
Eric Diaz <Eric.diaz13@yahoo.com>
2019-03-05 00:25:33 -0800

    Error handling

--- flask_app.py
+++ flask_app.py
@@ -5,22 +5,30 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def ret_none():
     return "No data available\n";
+
+
+@app.errorhandler(404)
+def get404d(num):
+
+    return "Got that 404, bro"
+
 
 @app.route('/<variable>')
 def ret_coords(variable):
 
     print(variable)
-    #Read the formatted data and compare it to the GPS coordinates
+    # Read the formatted data and compare it to the GPS coordinates
     with open("json_updated.json") as f:
         json_data = f.read()
 
-    #convert the json string into a dictionary
+    # convert the json string into a dictionary
     json_data = json.loads(json_data)
 
-    #get the latitude and longitude from the URL with ',' as a delimiter
+    # get the latitude and longitude from the URL with ',' as a delimiter
     lat, lon = variable.split(',')
 
     print(lat)
@@ -32,15 +40,17 @@ def ret_coords(variable):
     j = lat+lon
 
     new_json = dict()
-    #set default values for JSON
+    # set default values for JSON
     new_json = {
-    "ASSAULT": 0, "MURDER": 0, "THEFT": 0, "RAPE": 0, "GTA": 0, "ROBBERY": 0, "OTHER": 0
+        "ASSAULT": 0, "MURDER": 0, "THEFT": 0, "RAPE": 0, "GTA": 0, "ROBBERY": 0, "OTHER": 0
     }
     new_json = json.dumps(new_json)
-    #checks if there's a coordinates stored
+
+# checks if there's a coordinates stored
     if j in json_data:
         new_json = json.dumps(json_data[j])
     return new_json
+
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)

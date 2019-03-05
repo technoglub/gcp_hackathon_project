5816a65f6797efae112665ab62a39b48325ef26a
Eric Diaz <Eric.diaz13@yahoo.com>
2019-03-05 12:40:18 -0800

    Assert response code 200 for every entry

--- tests/url_test.py
+++ tests/url_test.py
@@ -12,27 +12,34 @@ class test_url():
         self.good_lat = '34.10'
         self.good_lon = '-117.68'
 
+    def assert_200(self, r):
+        assert(r.status_code == 200)
+
     def test_empty(self):
         r = requests.get(self.url)
         txt = r.text
-
+        self.assert_200(r)
         assert(txt == "No data available\n")
 
     def test_no_comma(self):
         r = requests.get(self.url + self.bad_lat)
+        self.assert_200(r)
         text = r.text
         assert(text == "Got that comma problem!")
 
     def test_one_comma_bad_coords(self):
         r = requests.get(self.url + self.bad_lat + ',')
+        self.assert_200(r)
         assert(r.text == "There was an exception: could not convert string to float: ")
 
     def test_one_comma_good_coords(self):
         r = requests.get(self.url + self.good_lat + ',' + self.good_lon)
+        self.assert_200(r)
         assert(r.text == '{"ASSAULT": 0, "MURDER": 0, "THEFT": 0, "RAPE": 0, "GTA": 0, "ROBBERY": 0, "OTHER": 1}')
 
     def test_two_comma(self):
         r = requests.get(self.url + self.good_lon +',' + self.good_lat + ',')
+        self.assert_200(r)
         assert(r.text == "Got that comma problem!")
 
 

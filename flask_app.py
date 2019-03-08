618aa6dc6f27e4424896bab6e790b3eb287b9d8a
Eric Diaz <Eric.diaz13@yahoo.com>
2019-03-07 17:58:05 -0800

    Changed to port 80, added some print statements.

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 from flask import Flask
 import json
-import re
 
 app = Flask(__name__)
 
@@ -37,7 +36,6 @@ def ret_coords(variable):
     if not ',' in variable:
         return "Invalid format"
 
-    print(variable)
     # Read the formatted data and compare it to the GPS coordinates
     with open("json_updated.json") as f:
         json_data = f.read()
@@ -48,8 +46,6 @@ def ret_coords(variable):
     # get the latitude and longitude from the URL with ',' as a delimiter
     lat, lon = variable.split(',')
 
-    print(lat)
-    print(lon)
     try:
         lat = "{0:.2f}".format(float(lat))
         lon = "{0:.2f}".format(float(lon))
---
+++ tests/stress_test.py
@@ -0,0 +1,47 @@
+import threading
+import requests
+from _datetime import datetime
+import random
+import queue
+
+
+class StressTester():
+
+    def __init__(self, url="http://LOCALHOST:5000/"):
+        self.url = url
+        self.port = 5000
+        self.threads = []
+        self.q = queue.Queue()
+
+
+    def stress_test(self):
+        '''Creates a bunch of threads to spam the servers'''
+
+        for i in range(128):
+            t = threading.Thread(target=self.make_request)
+            t.start()
+
+
+    def make_request(self):
+        start = datetime.now()
+        indx = random.randint(0,5)
+        print (indx)
+        locs = ["34.13,-117.27", "34.07,-117.28", "34.52,-117.43", "34.52,-117.43", "34.08,-117.24", "34.10,-117.28"]
+        print(locs[indx])
+        r = requests.get(self.url + locs[indx])
+        time = datetime.now() - start
+        self.q.put(item=time, block=True)
+
+
+def main():
+    tester = StressTester()
+    tester.stress_test()
+    a = []
+    while not tester.q.empty():
+        a.append(tester.q.get())
+
+    for i in a:
+        print (i)
+
+
+main()
